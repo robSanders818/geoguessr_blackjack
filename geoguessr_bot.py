157911f41815f47b376675080d7935e69031fda1
robSanders818 <sanders.rob@husky.neu.edu>
2020-10-06 18:23:53 -0400

    added scores to message

--- geoguessr_bot.py
+++ geoguessr_bot.py
@@ -24,26 +24,29 @@ class DiscordClient(discord.Client):
             if message.author.id not in self.games:
                 self.games[message.author.id] = ()
             try:
-                results = geoguessr_blackjack(message.content, self.games[message.author.id][0])
+                results = geoguessr_blackjack(
+                    message.content, self.games[message.author.id][0] if self.games[message.author.id] else []
+                )
                 if len(results[0]) == 0:
                     player_message = 'No players fit between these scores, try another round.'
                 else:
                     self.games[message.author.id] = results
-                    player_message = 'Players in game still:\n\n' + '\n'.join(self.games[message.author.id][0])
+                    temp_res = list(zip(results[0], results[1]))
+                    temp_res = [score[0] + ' | ' + str('{:,}'.format(score[1])) for score in temp_res]
+                    player_message = 'Players in game still:\n\n' + '\n'.join(temp_res)
             except ValueError:
                 player_message = (
                         'Incorrect Formatting- Please use this format:\nhttps://www.geoguessr.com/results/ 5000 ' +
                         '15000\n\nNote there are no hypens, no commas, and send the lower score then the higher' +
                         'score.'
                 )
-        if len(player_message) > 1999:
-            with open('{}.csv'.format(message.author.id), 'w', encoding='utf-8') as result_file:
-                wr = csv.writer(result_file, dialect='excel')
-                for name, score in zip(self.games[message.author.id][0], self.games[message.author.id][1]):
-                    wr.writerow([name, score])
-            await message.channel.send('There are too many players left to fit in one message, here they are:', file=discord.File('{}.csv'.format(message.author.id)))
-        else:
+        if len(player_message) <= 1999:
             await message.channel.send(player_message)
+        with open('{}.csv'.format(message.author.id), 'w', encoding='utf-8') as result_file:
+            wr = csv.writer(result_file, dialect='excel')
+            for name, score in zip(self.games[message.author.id][0], self.games[message.author.id][1]):
+                wr.writerow([name, '{:,}'.format(score)])
+        await message.channel.send('Here are the results for this round:', file=discord.File('{}.csv'.format(message.author.id)))
         await message.channel.send('If you want to start a new game, send !new')
 
 
