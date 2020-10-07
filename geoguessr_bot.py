23da498e3ed54333674f17f169de59a1e847ba53
Robert Sanders <45695926+robSanders818@users.noreply.github.com>
2020-10-07 00:52:59 -0400

    updated to have top n scores, and top n% scores

--- geoguessr_bot.py
+++ geoguessr_bot.py
@@ -31,7 +31,7 @@ class DiscordClient(discord.Client):
                     player_message = 'No players fit between these scores, try another round.'
                 else:
                     self.games[message.author.id] = results
-                    temp_res = list(zip(results[0], results[1]))
+                    temp_res = list(zip(results[1], results[2]))
                     temp_res = [score[0] + ' | ' + str('{:,}'.format(score[1])) for score in temp_res]
                     player_message = 'Players in game still:\n\n' + '\n'.join(temp_res)
             except ValueError:
@@ -42,11 +42,12 @@ class DiscordClient(discord.Client):
                 )
         if len(player_message) <= 1999:
             await message.channel.send(player_message)
-        with open('{}.csv'.format(message.author.id), 'w', encoding='utf-8') as result_file:
-            wr = csv.writer(result_file, dialect='excel')
-            for name, score in zip(self.games[message.author.id][0], self.games[message.author.id][1]):
-                wr.writerow([name, '{:,}'.format(score)])
-        await message.channel.send('Here are the results for this round:', file=discord.File('{}.csv'.format(message.author.id)))
+        if self.games[message.author.id] and len(self.games[message.author.id][0]) > 0:
+            with open('{}.csv'.format(message.author.id), 'w', encoding='utf-8') as result_file:
+                wr = csv.writer(result_file, dialect='excel')
+                for name, score in zip(self.games[message.author.id][1], self.games[message.author.id][2]):
+                    wr.writerow([name, '{:,}'.format(score)])
+            await message.channel.send('Here are the results for this round:', file=discord.File('{}.csv'.format(message.author.id)))
         await message.channel.send('If you want to start a new game, send !new')
 
 
--- geoguessr_download.py
+++ geoguessr_download.py
@@ -10,13 +10,22 @@ import json
 
 # Automated Program to download geoguessr results table, to play blackjack with
 def geoguessr_blackjack(message, players):
-    url, lower_score, higher_score = message.split(' ')
-    lower_score, higher_score = int(lower_score), int(higher_score)
-    if lower_score > higher_score:
-        raise ValueError
+    message_array = message.split(' ')
+    url = message_array[0]
+    lower_score, higher_score, percent, cut_off = None, None, None, None
+    if '%' in message_array[1]:
+        percent = message_array[1]
+        percent = int(percent[:-1])
+    elif len(message_array) == 2:
+        cut_off = int(message_array[1])
+    else:
+        lower_score, higher_score = message_array[1], message_array[2]
+        lower_score, higher_score = int(lower_score), int(higher_score)
+        if lower_score > higher_score:
+            raise ValueError
     url = 'https://geoguessr.com/api/v3/results/scores{}/0/10000'.format(url.split('results')[1])
     all_scores = retrieve_all_scores(url)
-    players = filter_player_scores(all_scores, players, lower_score, higher_score)
+    players = filter_player_scores(all_scores, players, lower_score, higher_score, percent, cut_off)
     return players
 
 
@@ -25,18 +34,20 @@ def retrieve_all_scores(url) -> pd.DataFrame:
     html = requests.get(url).content
     df = pd.DataFrame(
         json.loads(html)
-    ).drop(columns=['gameToken', 'userId', 'isActive', 'isLeader', 'pinUrl', 'color'])
+    ).drop(columns=['gameToken', 'isActive', 'isLeader', 'pinUrl', 'color'])
     return df
 
 
 # filters player scores based on player already existing in game, and if their score was correct
-def filter_player_scores(all_scores: pd.DataFrame, players: List, lower_score: int, higher_score: int) -> Tuple[List[str], List[str]]:
+def filter_player_scores(
+        all_scores: pd.DataFrame, players: List, lower_score: int, higher_score: int, percent: int, cut_off: int
+) -> Tuple[List[str], List[str], List[str]]:
     if len(players) > 0:
-        player_filter = all_scores['playerName'].isin(players)
+        player_filter = all_scores['userId'].isin(players)
         all_scores = all_scores[player_filter]
 
     # deals with final round logic to find closest player to score
-    if lower_score == higher_score:
+    if lower_score is not None and higher_score is not None and lower_score == higher_score:
         all_scores.loc[:, 'diff'] = all_scores.apply(
             lambda row: abs(row['totalScore'] - lower_score),
             axis=1
@@ -47,7 +58,11 @@ def filter_player_scores(all_scores: pd.DataFrame, players: List, lower_score: i
         filter_lower = all_scores['totalScore'] == (lower_score - min_diff)
         all_scores = all_scores[filter_higher | filter_lower]
 
-    lower_filter = all_scores['totalScore'] >= lower_score
-    higher_filter = all_scores['totalScore'] <= higher_score
-    all_scores = all_scores[lower_filter & higher_filter]
-    return list(all_scores['playerName']), list(all_scores['totalScore'])
+    if percent is not None or cut_off is not None:
+        cut_off = len(all_scores) * (percent / 100) if cut_off is None else cut_off
+        all_scores = all_scores.head(round(cut_off))
+    else:
+        lower_filter = all_scores['totalScore'] >= lower_score
+        higher_filter = all_scores['totalScore'] <= higher_score
+        all_scores = all_scores[lower_filter & higher_filter]
+    return list(all_scores['userId']), list(all_scores['playerName']), list(all_scores['totalScore'])
