cc2af6e43ac2a575946fd85d9046a88e0248d0e7
Robert Sanders <45695926+robSanders818@users.noreply.github.com>
2020-10-31 00:42:25 -0400

    added start/stop commadns to stop the bot from responding/porccessing games

--- geoguessr_bot.py
+++ geoguessr_bot.py
@@ -9,6 +9,7 @@ class DiscordClient(discord.Client):
     def __init__(self):
         super().__init__()
         self.games = {}
+        self.pause = {}
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
@@ -36,33 +37,38 @@ class DiscordClient(discord.Client):
             if message.author.id not in self.games and message.channel.id not in self.games:
                 game_id = message.author.id
                 self.games[message.author.id] = ()
-            try:
-                results = geoguessr_blackjack(
-                    message.content, self.games[game_id][0] if self.games[game_id] else []
-                )
-                if len(results[0]) == 0:
-                    player_message = 'No players fit between these scores, try another round.'
-                else:
-                    self.games[game_id] = results
-                    temp_res = list(zip(results[1], results[2]))
-                    temp_res = [score[0] + ' | ' + str('{:,}'.format(score[1])) for score in temp_res]
-                    player_message = 'Players in game still:\n\n' + '\n'.join(temp_res)
-            except ValueError:
-                player_message = (
-                        'Incorrect Formatting- Please use this format:\nhttps://www.geoguessr.com/results/ 5000 ' +
-                        '15000\n\nNote there are no hypens, no commas, and send the lower score then the higher' +
-                        'score.\n You can also send in just one number (n), and that will find the top (n) scorers, or ' +
-                        'send a percent (n), and it will send top (n) percent of scorers.'
-                )
-            if len(player_message) <= 1999:
-                await message.channel.send(player_message)
-            if self.games[game_id] and len(self.games[game_id][0]) > 0:
-                with open('{}.csv'.format(game_id), 'w', encoding='utf-8') as result_file:
-                    wr = csv.writer(result_file, dialect='excel')
-                    for name, score in zip(self.games[game_id][1], self.games[game_id][2]):
-                        wr.writerow([name, '{:,}'.format(score)])
-                await message.channel.send('Here are the results for this round:', file=discord.File('{}.csv'.format(game_id)))
-            await message.channel.send('If you want to start a new game, send !new')
+            if message.content == '!stop':
+                self.pause[game_id] = True
+            if message.content == '!start':
+                self.pause[game_id] = False
+            if game_id not in self.pause or not self.pause[game_id]:
+                try:
+                    results = geoguessr_blackjack(
+                        message.content, self.games[game_id][0] if self.games[game_id] else []
+                    )
+                    if len(results[0]) == 0:
+                        player_message = 'No players fit between these scores, try another round.'
+                    else:
+                        self.games[game_id] = results
+                        temp_res = list(zip(results[1], results[2]))
+                        temp_res = [score[0] + ' | ' + str('{:,}'.format(score[1])) for score in temp_res]
+                        player_message = 'Players in game still:\n\n' + '\n'.join(temp_res)
+                except ValueError:
+                    player_message = (
+                            'Incorrect Formatting- Please use this format:\nhttps://www.geoguessr.com/results/ 5000 ' +
+                            '15000\n\nNote there are no hypens, no commas, and send the lower score then the higher' +
+                            'score.\n You can also send in just one number (n), and that will find the top (n) scorers, or ' +
+                            'send a percent (n), and it will send top (n) percent of scorers.'
+                    )
+                if len(player_message) <= 1999:
+                    await message.channel.send(player_message)
+                if self.games[game_id] and len(self.games[game_id][0]) > 0:
+                    with open('{}.csv'.format(game_id), 'w', encoding='utf-8') as result_file:
+                        wr = csv.writer(result_file, dialect='excel')
+                        for name, score in zip(self.games[game_id][1], self.games[game_id][2]):
+                            wr.writerow([name, '{:,}'.format(score)])
+                    await message.channel.send('Here are the results for this round:', file=discord.File('{}.csv'.format(game_id)))
+                await message.channel.send('If you want to start a new game, send !new')
 
 
 client = DiscordClient()
